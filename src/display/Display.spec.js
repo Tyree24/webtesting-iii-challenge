e281988f380fc9037a6f1e0c6ef6f643308ce2b2
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-12-04 21:18:11 -0600

    add default display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+test("it should default to 'unlocked' and 'open'", () => {
+  const { getByText } = render(<Display />);
+
+  expect(getByText(/unlocked/i));
+  expect(getByText(/open/i));
+});
